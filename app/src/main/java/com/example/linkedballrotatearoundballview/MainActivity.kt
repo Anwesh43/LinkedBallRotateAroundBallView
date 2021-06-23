9b29507babe7df0cf2567014c5e93568a322d093
Anwesh43 <anweshthecool0@gmail.com>
2021-06-24 03:34:46 +0530

    adding view in activity and fullscreen support

--- app/src/main/java/com/example/linkedballrotatearoundballview/MainActivity.kt
+++ app/src/main/java/com/example/linkedballrotatearoundballview/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.linkedballrotatearoundballview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.example.ballrotatearoundballview.BallRotateAroundBallView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        BallRotateAroundBallView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
+
 }
